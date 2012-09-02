e69202545d7fc09f8c9a3313d9c227f2ad87e4e7
Avraham Shukron <avraham.shukron@gmail.com>
2012-09-02 15:16:08 +0300

    Fixed major bug with interactivity of items inside a SwipeView
    changed the scrollView frame to be exactly the SwipeView bounds.

--- SwipeView/SwipeView.m
+++ SwipeView/SwipeView.m
@@ -411,14 +411,14 @@
         {
             if (_vertical)
             {
-                frame = CGRectMake(0.0f, (self.frame.size.height - _itemSize.height * _itemsPerPage)/2.0f,
-                                   self.frame.size.width, _itemSize.height * _itemsPerPage);
+                //                frame = CGRectMake(0.0f, (self.frame.size.height - _itemSize.height * _itemsPerPage)/2.0f,
+                //                                   self.frame.size.width, _itemSize.height * _itemsPerPage);
                 contentSize.height = _itemSize.height * _numberOfItems;
             }
             else
             {
-                frame = CGRectMake((self.frame.size.width - _itemSize.width * _itemsPerPage)/2.0f,
-                                   0.0f, _itemSize.width * _itemsPerPage, self.frame.size.height);
+                //                frame = CGRectMake((self.frame.size.width - _itemSize.width * _itemsPerPage)/2.0f,
+                //                                   0.0f, _itemSize.width * _itemsPerPage, self.frame.size.height);
                 contentSize.width = _itemSize.width * _numberOfItems;
             }
             break;
@@ -427,12 +427,12 @@
         {
             if (_vertical)
             {
-                frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, _itemSize.height * _itemsPerPage);
+                //                frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, _itemSize.height * _itemsPerPage);
                 contentSize.height = _itemSize.height * _numberOfItems - (self.frame.size.height - frame.size.height);
             }
             else
             {
-                frame = CGRectMake(0.0f, 0.0f, _itemSize.width * _itemsPerPage, self.frame.size.height);
+                //                frame = CGRectMake(0.0f, 0.0f, _itemSize.width * _itemsPerPage, self.frame.size.height);
                 contentSize.width = _itemSize.width * _numberOfItems - (self.frame.size.width - frame.size.width);
             }
             break;
